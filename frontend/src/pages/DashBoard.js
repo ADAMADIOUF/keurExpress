a870b00415a34c40ccfe6a1eb7588f10ee8e2d8f
adama diouf <adamadiouf2017@gmail.com>
2025-01-01 14:01:23 -0600

    add files

--- frontend/src/pages/DashBoard.js
+++ frontend/src/pages/DashBoard.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { useGetProperiesQuery } from '../slices/propertieSlice'
 
 const DashBoard = () => {
   return (
